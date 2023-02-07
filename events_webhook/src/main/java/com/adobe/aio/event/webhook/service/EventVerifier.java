879d613ee5192cc187f4380ab79ceba36bf3639a
Francois Le Droff <ledroff@adobe.com>
2023-02-07 15:17:32 +0100

    GH-125 fixed a couple of bugs around event verifications
    while improving the junit code coverage

--- events_webhook/src/main/java/com/adobe/aio/event/webhook/service/EventVerifier.java
+++ events_webhook/src/main/java/com/adobe/aio/event/webhook/service/EventVerifier.java
@@ -14,11 +14,9 @@ package com.adobe.aio.event.webhook.service;
 import static java.nio.charset.StandardCharsets.UTF_8;
 
 import com.adobe.aio.event.webhook.feign.FeignPubKeyService;
-import com.adobe.aio.exception.AIOException;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import java.security.GeneralSecurityException;
 import java.security.Signature;
 import java.util.Base64;
 import java.util.Map;
@@ -28,7 +26,7 @@ import org.slf4j.LoggerFactory;
 
 public class EventVerifier {
 
-  private Logger logger = LoggerFactory.getLogger(this.getClass());
+  private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
   public static final String RECIPIENT_CLIENT_ID = "recipient_client_id";
 
@@ -41,8 +39,8 @@ public class EventVerifier {
   private final FeignPubKeyService pubKeyService;
 
   /**
-   * Used for TESTING ONLY
-   * for creating instance with a test stub url
+   * Used for TESTING ONLY for creating instance with a test stub url
+   *
    * @param url
    */
   EventVerifier(String url) {
@@ -54,15 +52,15 @@ public class EventVerifier {
   }
 
   /**
-   * @param eventPayload    the event payload to verify
-   * @param apiKey          the event payload `recipient_client_id` must be matching it
-   * @param requestHeaders  webhook request requestHeaders sent along the event payload: containing
-   *                        the path to the two public keys and the associated signatures of the
-   *                        eventPayload. Indeed, Adobe I/O Events sends two signatures, either of
-   *                        which is valid at any point of time (even when the signatures are rotated).
-   *                        So, the signed payload is considered valid if any one of the signatures is valid.
-   *                        Refer our public doc for more details -
-   *                        https://developer.adobe.com/events/docs/guides/#security-considerations
+   * @param eventPayload   the event payload to verify
+   * @param apiKey         the event payload `recipient_client_id` must be matching it
+   * @param requestHeaders webhook request requestHeaders sent along the event payload: containing
+   *                       the path to the two public keys and the associated signatures of the
+   *                       eventPayload. Indeed, Adobe I/O Events sends two signatures, either of
+   *                       which is valid at any point of time (even when the signatures are
+   *                       rotated). So, the signed payload is considered valid if any one of the
+   *                       signatures is valid. Refer our public doc for more details -
+   *                       https://developer.adobe.com/events/docs/guides/#security-considerations
    * @return the security verification result
    */
   public boolean verify(String eventPayload, String apiKey, Map<String, String> requestHeaders) {
@@ -88,21 +86,21 @@ public class EventVerifier {
     return verifySignature(eventPayload, headers.get(ADOBE_IOEVENTS_PUB_KEY_1_PATH),
         headers.get(ADOBE_IOEVENTS_DIGI_SIGN_1)) ||
         verifySignature(eventPayload, headers.get(ADOBE_IOEVENTS_PUB_KEY_2_PATH),
-            headers.get(ADOBE_IOEVENTS_DIGI_SIGN_1));
+            headers.get(ADOBE_IOEVENTS_DIGI_SIGN_2));
   }
 
   private boolean verifySignature(String eventPayload, String publicKeyPath, String signature) {
-    byte[] data = eventPayload.getBytes(UTF_8);
     try {
       // signature generated at I/O Events side is Base64 encoded, so it must be decoded
       byte[] sign = Base64.getDecoder().decode(signature);
       Signature sig = Signature.getInstance("SHA256withRSA");
       sig.initVerify(pubKeyService.getAioEventsPublicKey(publicKeyPath));
-      sig.update(data);
+      sig.update(eventPayload.getBytes(UTF_8));
       return sig.verify(sign);
-    } catch (GeneralSecurityException e) {
-      throw new AIOException("Error verifying signature for public key " + publicKeyPath
-          + ". Reason -> " + e.getMessage(), e);
+    } catch (Exception e) {
+      logger.error(
+          "Invalid signature for public key " + publicKeyPath + ". Reason -> " + e.getMessage(), e);
+      return false;
     }
   }
 
@@ -114,7 +112,7 @@ public class EventVerifier {
       return (recipientClientIdNode != null && recipientClientIdNode.textValue() != null
           && recipientClientIdNode.textValue().equals(clientId));
     } catch (JsonProcessingException e) {
-      logger.error("Invalid event Payload: {}",e.getMessage());
+      logger.error("Invalid event Payload: {}", e.getMessage());
       return false;
     }
   }
