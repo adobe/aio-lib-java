236d5dc1653ad9bb324a67123d9ab3f6e18b89bc
Bryan Stopp <bryan.stopp@gmail.com>
2023-08-03 14:36:51 -0500

    Extract JWT context/credentials

--- core/src/test/java/com/adobe/aio/workspace/WorkspaceTest.java
+++ core/src/test/java/com/adobe/aio/workspace/WorkspaceTest.java
@@ -11,56 +11,137 @@
  */
 package com.adobe.aio.workspace;
 
-import com.adobe.aio.workspace.Workspace;
-import com.adobe.aio.util.Constants;
 import java.io.IOException;
-import org.junit.Assert;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.ExpectedException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+
+import com.adobe.aio.auth.Context;
+import com.adobe.aio.auth.JwtContext;
+import com.adobe.aio.util.Constants;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class WorkspaceTest {
 
-  @Rule
-  public ExpectedException expectedEx = ExpectedException.none();
-
+  private static Workspace expected;
   private static final String TEST_PROPERTIES = "workspace.properties";
   private static final String TEST_VALUE = "_changeMe";
+  private static PrivateKey privateKey;
 
-  private static Workspace getTestWorkspaceFromProperties() {
-    return Workspace.builder()
-        .propertiesPath(TEST_PROPERTIES)
-        .build();
+  @BeforeAll
+  public static void beforeClass() throws Exception {
+    KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
+    kpg.initialize(2048);
+    KeyPair kp = kpg.generateKeyPair();
+    privateKey = kp.getPrivate();
+    expected = Workspace.builder().propertiesPath(TEST_PROPERTIES).privateKey(privateKey).build();
   }
 
   @Test
-  public void testProperties() throws IOException {
-    Workspace fromProperties = getTestWorkspaceFromProperties();
-    Workspace expected = Workspace.builder()
+  public void properties() throws IOException {
+
+    class MockContext implements Context {
+      @Override
+      public void validate() {
+
+      }
+    }
+
+    Workspace actual = Workspace.builder()
         .imsUrl(Constants.IMS_URL)
+        .imsOrgId(Workspace.IMS_ORG_ID + TEST_VALUE)
         .apiKey(Workspace.API_KEY + TEST_VALUE)
-        .clientSecret(Workspace.CLIENT_SECRET + TEST_VALUE)
-        .apiKey(Workspace.API_KEY + TEST_VALUE)
-        .credentialId(Workspace.CREDENTIAL_ID + TEST_VALUE)
         .consumerOrgId(Workspace.CONSUMER_ORG_ID + TEST_VALUE)
         .projectId(Workspace.PROJECT_ID + TEST_VALUE)
         .workspaceId(Workspace.WORKSPACE_ID + TEST_VALUE)
-        .imsOrgId(Workspace.IMS_ORG_ID + TEST_VALUE)
-        .technicalAccountId(Workspace.TECHNICAL_ACCOUNT_ID + TEST_VALUE)
-        .addMetascope(Workspace.META_SCOPES + TEST_VALUE)
+        .authContext(new MockContext())
         .build();
-    Assert.assertEquals(expected, fromProperties);
-    Assert.assertEquals(expected.hashCode(), fromProperties.hashCode());
-    Assert.assertEquals(expected.toString(), fromProperties.toString());
-    fromProperties.validateWorkspaceContext();
+    assertEquals(actual, expected);
+    assertEquals(actual.hashCode(), expected.hashCode());
+    assertEquals(actual.toString(), expected.toString());
+    actual.validateAll();
   }
 
   @Test
-  public void testValidateJwtCredentialConfig() throws IOException {
-    Workspace workspace = getTestWorkspaceFromProperties();
-    expectedEx.expect(IllegalArgumentException.class);
-    expectedEx.expectMessage("Your `Worskpace` is missing a privateKey");
-    workspace.validateJwtCredentialConfig();
+  public void missingImsOrg() {
+    Workspace actual = Workspace.builder().build();
+    Exception ex = assertThrows(IllegalStateException.class, actual::validateWorkspaceContext);
+    assertEquals("Your `Workspace` is missing an imsOrgId", ex.getMessage());
   }
 
+  @Test
+  public void missingConsumerOrgId() {
+    Workspace actual = Workspace.builder()
+        .imsOrgId(Workspace.IMS_ORG_ID + TEST_VALUE)
+        .build();
+    Exception ex = assertThrows(IllegalStateException.class, actual::validateWorkspaceContext);
+    assertEquals("Your `Workspace` is missing a consumerOrgId", ex.getMessage());
+  }
+
+  @Test
+  public void missingApiKey() {
+    Workspace actual = Workspace.builder()
+        .imsOrgId(Workspace.IMS_ORG_ID + TEST_VALUE)
+        .consumerOrgId(Workspace.CONSUMER_ORG_ID + TEST_VALUE)
+        .build();
+    Exception ex = assertThrows(IllegalStateException.class, actual::validateWorkspaceContext);
+    assertEquals("Your `Workspace` is missing an apiKey", ex.getMessage());
+  }
+
+  @Test
+  public void missingProjectId() {
+    Workspace actual = Workspace.builder()
+        .imsOrgId(Workspace.IMS_ORG_ID + TEST_VALUE)
+        .consumerOrgId(Workspace.CONSUMER_ORG_ID + TEST_VALUE)
+        .apiKey(Workspace.API_KEY + TEST_VALUE)
+        .build();
+    Exception ex = assertThrows(IllegalStateException.class, actual::validateWorkspaceContext);
+    assertEquals("Your `Workspace` is missing a projectId", ex.getMessage());
+  }
+
+  @Test
+  public void missingWorkspaceId() {
+    Workspace actual = Workspace.builder()
+        .imsOrgId(Workspace.IMS_ORG_ID + TEST_VALUE)
+        .consumerOrgId(Workspace.CONSUMER_ORG_ID + TEST_VALUE)
+        .apiKey(Workspace.API_KEY + TEST_VALUE)
+        .projectId(Workspace.PROJECT_ID + TEST_VALUE)
+        .build();
+    Exception ex = assertThrows(IllegalStateException.class, actual::validateWorkspaceContext);
+    assertEquals("Your `Workspace` is missing a workspaceId", ex.getMessage());
+  }
+  
+  @Test
+  public void projectUrl() {
+    Workspace actual = Workspace.builder()
+        .consumerOrgId(Workspace.CONSUMER_ORG_ID + TEST_VALUE)
+        .projectId(Workspace.PROJECT_ID + TEST_VALUE)
+        .build();
+    
+    assertEquals("https://developer.adobe.com/console/projects/aio_consumer_org_id_changeMe/aio_project_id_changeMe/overview", actual.getProjectUrl());
+  }
+
+  @Test
+  public void jwtBackwardsCompat() throws Exception {
+    Workspace actual = Workspace.builder()
+        .imsUrl(Constants.IMS_URL)
+        .imsOrgId(Workspace.IMS_ORG_ID + TEST_VALUE)
+        .apiKey(Workspace.API_KEY + TEST_VALUE)
+        .consumerOrgId(Workspace.CONSUMER_ORG_ID + TEST_VALUE)
+        .projectId(Workspace.PROJECT_ID + TEST_VALUE)
+        .workspaceId(Workspace.WORKSPACE_ID + TEST_VALUE)
+        .clientSecret(JwtContext.CLIENT_SECRET + TEST_VALUE)
+        .credentialId(JwtContext.CREDENTIAL_ID + TEST_VALUE)
+        .technicalAccountId(JwtContext.TECHNICAL_ACCOUNT_ID + TEST_VALUE)
+        .privateKey(privateKey)
+        .addMetascope(JwtContext.META_SCOPES + TEST_VALUE)
+        .build();
+    assertEquals(actual, expected);
+    assertEquals(actual.hashCode(), expected.hashCode());
+    assertEquals(actual.toString(), expected.toString());
+    actual.validateAll();
+  }
 }
