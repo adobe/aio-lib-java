6b3e1a28b916214370c354e48cd4cad380b93e4e
abhupadh <>
2023-10-10 17:50:31 -0700

    reverting package refactoring for EventVerifier

--- events_webhook/src/main/java/com/adobe/aio/event/webhook/service/EventVerifier.java
+++ events_webhook/src/main/java/com/adobe/aio/event/webhook/service/EventVerifier.java
@@ -9,7 +9,7 @@
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
-package com.adobe.aio.event.webhook.service.impl;
+package com.adobe.aio.event.webhook.service;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 
